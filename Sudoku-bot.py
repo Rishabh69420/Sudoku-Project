f2235f1377b71e241d69adf5ffb9dca7e8875b0d
Rishabh69420 <65121903+Rishabh69420@users.noreply.github.com>
2020-07-01 20:33:10 +0400

    Changed plan
    Making the algorithm to generate a puzzle too

--- Sudoku-bot.py
+++ Sudoku-bot.py
@@ -1,6 +1,7 @@
 #this is the start of the bot
 """
 The First goal is to create classes for different methods of solving a cell in a sudoku puzzle
-The Second one is to start the development of the algorithms to olve each cell based on the classes
-The Third goal is to create a user friendly GUI to input a puzzle, so that the bot can solve it
+The Second goal is to start the development of the algorithms to solve each cell based on the classes
+The Third goal is to create an algorithm to *generate* sudoku puzzles
+The Fourth goal is to create a user friendly GUI to input the solution to the puzzle, and compare it with the computer generated answers
 """
