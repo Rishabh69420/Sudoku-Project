0c505dbf03fd3f02518d9b094a223f7be7a0a6a2
Rishabh69420 <65121903+Rishabh69420@users.noreply.github.com>
2020-07-01 16:35:19 +0400

    Create Sudoku-bot.py

---
+++ Sudoku-bot.py
@@ -0,0 +1,6 @@
+#this is the start of the bot
+"""
+The First goal is to create classes for different methods of solving a cell in a sudoku puzzle
+The Second one is to start the development of the algorithms to olve each cell based on the classes
+The Third goal is to create a user friendly GUI to input a puzzle, so that the bot can solve it
+"""
